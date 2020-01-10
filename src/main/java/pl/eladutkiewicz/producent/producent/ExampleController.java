c90c7189a58aa7b4b4a3b876fbf2a901331031da
e.n.dutkiewicz@gmail.com <e.n.dutkiewicz@gmail.com>
2020-01-10 19:27:53 +0100

    initial commit

---
+++ src/main/java/pl/eladutkiewicz/producent/producent/ExampleController.java
@@ -0,0 +1,8 @@
+package pl.eladutkiewicz.producent.producent;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// żeby aplikacja się nie zamykała od razu po włączeniu
+public class ExampleController {
+}
--- src/main/java/pl/eladutkiewicz/producent/producent/ProducentApplication.java
+++ src/main/java/pl/eladutkiewicz/producent/producent/ProducentApplication.java
@@ -1,13 +1,54 @@
 package pl.eladutkiewicz.producent.producent;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.jms.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 public class ProducentApplication {
+    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducentApplication.class, args);
-	}
+
+    public static void sendMessages() {
+
+        executorService.execute(() -> {
+            try {
+                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61618");
+                Connection connection = connectionFactory.createConnection();
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                Topic topic = session.createTopic("EXAMPLE_TOPIC");
+                connection.start();
+                int messageNumber = 0;
+                while (true) {
+                    Thread.sleep(2000);
+                    String message = "Wiadomość nr: "+ messageNumber;
+                    TextMessage textMessage = session.createTextMessage(message);
+                    MessageProducer messageProducer = session.createProducer(topic);
+                    messageProducer.send(textMessage);
+                    messageNumber++;
+                    System.out.println("Wiadomośc została wysłana " + message);
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        });
+
+
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProducentApplication.class, args);
+        sendMessages();
+    }
 
 }
