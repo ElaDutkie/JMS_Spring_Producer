59bcf59516346b162adffd39d49ea62a125ac27f
e.n.dutkiewicz@gmail.com <e.n.dutkiewicz@gmail.com>
2020-01-10 18:14:35 +0100

    initial commit

---
+++ src/main/java/pl/eladutkiewicz/producent/producent/ProducentApplication.java
@@ -0,0 +1,13 @@
+package pl.eladutkiewicz.producent.producent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/eladutkiewicz/producent/producent/ProducentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.eladutkiewicz.producent.producent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
